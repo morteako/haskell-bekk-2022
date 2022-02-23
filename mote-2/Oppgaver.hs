45a6b265e795d2a078fe7ac6651235ae27d5bc65
Morten Kolstad <morten.kolstad@bekk.no>
2022-02-24 00:32:44 +0100

    fasit

--- mote-2/Oppgaver.hs
+++ mote-2/Oppgaver.hs
@@ -1,14 +1,23 @@
+{-# OPTIONS_GHC -Wincomplete-patterns #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+module Oppgaver where
+
+
+
+
 import qualified Data.List (sort)
 
 -- Parametric polymorphishm
 
---Implementer funksjonene utifra typene :
+-- Implementer funksjonene utifra typene :
 
 constant :: a -> b -> a
-constant = undefined
+constant a _ = a
 
 f :: (a -> b -> c) -> (a -> b) -> a -> c
-f = undefined
+f abc ab a = abc a $ ab a
+
 
 
 -- Type classes
@@ -17,24 +26,62 @@ f = undefined
 
 -- 1
 
--- Implementer Eq og Ord (valgfri, men den må gi ) for disse typene :
+-- Implementer Eq og Ord (valgfri hva slags ordning, men den må være en gyldig ordning ) for disse typene :
 
 
 data Sjanger = Jazz | Rock | Pop | Klassisk deriving Show 
 
+
+
+instance Eq Sjanger where
+    Jazz == Jazz = True
+    Rock == Rock = True
+    Pop == Pop = True
+    Klassisk == Klassisk = True
+    _ == _ = False
+
+-- Ordning basert på rekkefølgen de er ført opp. Som er det samme som deriving gjør
+instance Ord Sjanger where
+    Jazz <= _ = True
+    _ <= Jazz = False
+    Rock <= _ = True
+    _ <= Rock = False
+    Pop <= _ = True
+    _ <= Pop = False
+    Klassisk <= Klassisk = True
+
+
 data NumberedVal a = NumberedVal Int a deriving Show
+
+instance Eq a => Eq (NumberedVal a) where
+    NumberedVal i a == NumberedVal i' a' = i == i' && a == a'
+
+instance Ord a => Ord (NumberedVal a) where
+    NumberedVal i a <= NumberedVal i' a' 
+        | i < i' = True
+        | i == i' && a <= a' = True
+        | otherwise = False
 
 data Tree a b = Empty | NodeA a | NodeB b | Nodes (Tree a b) (Tree a b) 
     deriving Show
 
-
+instance (Eq a,Eq b) => Eq (Tree a b) where
+    Empty == Empty = True
+    NodeA a == NodeA a' = a == a'
+    NodeB b == NodeB b' = b == b'
+    Nodes l r == Nodes l' r' = l == l' && r == r'
+    _ == _ = False
 
 -- 2
 -- data Maybe a = Nothing | Just a
 
 -- Implementer en funksjon som gir indexen elementet finnes, eller Nothing hvis det ikke finnes.
 findIndex :: Eq a => a -> [a] -> Maybe Int
-findIndex = undefined
+findIndex a xs = go 0 xs
+    where
+        go _ [] = Nothing
+        go i (x:xs) | x == a = Just i
+        go i (_:xs) = go (i+1) xs
 
 -- 3
 
@@ -42,12 +89,22 @@ findIndex = undefined
 -- Hva skal funksjonen ha som type?
 -- Hva er den mest generelle typen?
 
-findMax = undefined
+findMax :: Ord a => [a] -> Maybe a
+findMax [] = Nothing
+findMax (x:xs) = case findMax xs of
+    Nothing -> Just x
+    Just x' -> Just $ max x x'
+
+-- kan også forenkles ved å bruke innebygget Ord for Maybe
+findMax' :: Ord a => [a] -> Maybe a
+findMax' [] = Nothing
+findMax' (x:xs) = Just x `max` findMax xs
 
 -- 4
 -- Egen type class
 
--- Sortable er en type class for typer der man kan sortere innholdet og også gi en liste med elementene 
+-- Sortable er en type class for typer der man kan sortere innholdet og også gi en liste med elementene
+-- Alle elementene har da samme type 
 -- den har funksjonene sort og asList
 -- sort "1432" ==> "1234"
 -- asList "1432" ==> "1432"
@@ -57,39 +114,61 @@ findMax = undefined
 -- Endre typene for sort og asList slik at til det de burde være
 
 class Sortable a where
-    sort :: a --ikke riktig type, endre den til noe som gir mening 
-    asList :: [Int] --ikke riktig type, endre den til noe som gir mening 
-
-
+    sort :: a -> a 
+    sortReverse :: a -> a
 
 -- dette burde alltid være sant : asList (sort xs) == List.sort (asList xs)
 -- Prøv å kom på en annen egenskap som alltid burde være sann
 
-Implementer for
-String
-[Int]
-(Int,Int)
-(Char,Char,Char)
+-- Implementer for
+-- [Char]
+-- [Int]
+-- (Int,Int)
+-- (Char,Char)
 
 -- bruk : Data.List.sort , som er importert
+
+-- instance Sortable [Char] where
+--     sort = Data.List.sort
+
+-- instance Sortable [Int] where
+--     sort = Data.List.sort
+
+
+-- instance Sortable (Int,Int) where
+--     sort (a,b) = case compare a b of
+--         LT -> (a,b)
+--         EQ -> (a,b)
+--         GT -> (b,a)
+
+-- instance Sortable (Char,Char) where
+--     sort (a,b) = case compare a b of
+--         LT -> (a,b)
+--         EQ -> (a,b)
+--         GT -> (b,a)
+
+
 
 
 -- 5
 -- Gjør instancene mer generelle ved å bruke contexts
 
+instance Ord a => Sortable [a] where
+    sort = Data.List.sort
+    sortReverse = reverse . Data.List.sort
+
+instance Ord a => Sortable (a,a) where
+    sort (a,b) = case compare a b of
+        LT -> (a,b)
+        EQ -> (a,b)
+        GT -> (b,a)
+
+    sortReverse ab = (b,a)
+        where
+            (a,b) = sort ab
+
 
 -- 6
 -- Legg til en funksjon : sortReverse
 
--- Tips til ting som kan brukes for å implementere:
--- sortOn
--- sortWith
--- Data.Ord.Down
-
-
 -- 7
--- Finn flere instancer
-
--- Finn minst 1 instance for en type fra standardlib
-
--- Lag minst en egen type og lag instance
