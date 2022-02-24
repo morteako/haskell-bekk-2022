c0a49cbc05df71aa422f817ba5f564d8dce3f97e
Morten Kolstad <morten.kolstad@bekk.no>
2022-02-24 16:04:24 +0100

    .

--- mote-2/Plan.hs
+++ mote-2/Plan.hs
@@ -1,16 +1,23 @@
+{-# LANGUAGE FlexibleInstances #-}
+
+import qualified Data.Set as Set
+
 -- # Møte 2 : Typer
 
 -- ## Parametric polymorphishm . Gjelder for alle typer
 -- # Typeparameter
 
-intsLength :: [Int] -> Int
-intsLength = undefined
+-- intsLength :: [Int] -> Int
+-- intsLength [] = 0
+-- intsLength (_:xs) = 1 + intsLength xs
 
-charsLength :: [Char] -> Int
-charsLength = undefined
+-- charsLength :: [Char] -> Int
+-- charsLength [] = 0
+-- charsLength (_:xs) = 1 + charsLength xs
 
 allLength :: [a] -> Int
-allLength = undefined
+allLength [] = 0
+allLength (_:xs) = 1 + allLength xs
 --allLength :: forall a . [a] -> Int
 
 --kan da instansiere allLength med spesifikke typer
@@ -18,37 +25,56 @@ allLength = undefined
 -- bruke @Int
 
 id :: a -> a
-id = undefined
+id x = x
 
 fst :: (a,b) -> a
-fst = undefined
+fst (a,b) = a
 
 -- Ikke noe instanceof i Haskell, så gir garantier
 
-
--- ## Forskjellig oppførsel for hver type
 
 -- ### == : equals 
 
 -- lett å lage en 
 boolEq :: Bool -> Bool -> Bool
-boolEq = undefined
+boolEq True True = True
+boolEq False False = True
+boolEq _ _ = False
+
+charEq = undefined
 
 stringEq :: String -> String -> Bool
-stringEq = undefined
+stringEq "" "" = True
+stringEq (x:xs) (y:ys) = charEq x y && stringEq xs ys
+stringEq _ _ = False
 
 -- men hva med en generell likhetssjekk. Da må vi ha generelle typer, altså typeparametere
 
-eq :: a -> a -> Bool
-eq = undefined
+-- eq :: a -> a -> Bool
+-- eq = undefined
 
 -- Hvorfor funker ikke det?
 
 -- Det er på en måte det motsatte av det vi vil ha
 
+-- ## Forskjellig oppførsel for hver type
 
 -- Vi trenger en slags interface Eq
 
+-- class Eq' a where
+--     eq :: a -> a -> Bool
+
+-- instance Eq' Bool where
+--     eq True True = True
+--     eq False False = True
+--     eq _ _ = False
+
+data Hei = HeiOgHopp | HoppOgHei deriving (Eq,Ord,Show,Read) 
+
+-- instance Eq Hei where
+--     HeiOgHopp == HeiOgHopp = True
+--     HoppOgHei == HoppOgHei = True
+--     _ == _ = False
 -- type class Eq
 
 -- instance
@@ -57,20 +83,53 @@ eq = undefined
 
 -- ### Show
 
+-- ### Superklasser : Eq => Ord
 
--- ### Superklasser : Ord
 
-
--- ### Minimal
-
--- ==   <---> /=
 
 -- ### Egenskaper
 
--- Ord
--- Refleksiv
--- Transetiv
+-- Eq - ekvivalensrelasjon
+-- Refleksiv : a == a = True
+-- Symmetrisk : a == b  --> b == a
+-- Transitiv : a == b && b == c -> a == c
+
+
+-- Ord <=
+-- Refleksiv a <= a = True
+-- Transitiv : a <= b && b <= c   -> a <= c
 -- Antisymmetrisk : a <= b og b <= a, da er a == b
+
+
+
+-- Type classes : typer til verdier
+
+class Default a where
+    value :: a
+
+instance Default [Char] where
+    value = "hei"
+
+instance Default Int where
+    value = 5
+
+instance Default Bool where
+    value = True
+
+-- Deriving
+
+-- Data.Set
+
+
+-- Read
+
+-- Coherence
+
+
+
+
+
+
 
 -- ### Monoids
 
