e1f32b6b5725fea851d91a1ced32a4f485f1ba33
Morten Kolstad <morten.kolstad@bekk.no>
2023-01-24 15:54:51 +0100

    noe

--- monadtrans/src/State/Main.hs
+++ monadtrans/src/State/Main.hs
@@ -2,20 +2,20 @@ module Main where
 
 import System.Environment (getArgs)
 
--- newtype State s a = State (s -> (a,s))
+newtype MyState s a = MyState (s -> (a, s))
 
 import Control.Monad.State
 import Data.Functor.Identity
 
 -- newtype State s a = State {runState :: s -> (a, s)}
 
-get' :: State s s
+get' :: MyState s s
 get' = undefined
 
-put' :: s -> State s ()
+put' :: s -> MyState s ()
 put' = undefined
 
-modify :: (s -> s) -> State s ()
+modify :: (s -> s) -> MyState s ()
 modify f = do
     s <- get
     put $ f s
@@ -24,29 +24,14 @@ modify f = do
 inc :: State Int Int
 inc = undefined
 
--- bruk inc til å putte tall som øker inn i treet
 
-data Tree a = Leaf a | Bin (Tree a) (Tree a)
-
--- size :: Tree a -> S
-
--- andThen :: (a -> State s b) -> State s a -> State s b
--- --Stack-operasjoner
 -- push :: Int -> State [Int] ()
 -- pop :: State [Int] Int
 -- -- lage en funksjon som tar manipulerer staten (altså en stack) ved å legge øke øverste tall med 1
 
 -- incTop :: State [Int] ()
--- incTop =
---     pop
---         & andThen (\poppedNum -> push (poppedNum + 1))
+-- incTop = undefined
 
--- --med fmap
--- incTop :: State [Int] ()
--- incTop =
---     pop
---         & fmap (+ 1)
---         & andThen push
 
 main :: IO ()
 main = do
--- monadtrans/src/Trans/Main.hs
+++ monadtrans/src/Trans/Main.hs
@@ -12,7 +12,9 @@ import Text.Read (readMaybe)
 -- intro reader
 
 -- lese to tall fra bruker i loop
+
 -- gi liste fra tall..tall
+
 -- legge på feilmelding
 
 -- Either
@@ -21,42 +23,13 @@ import Text.Read (readMaybe)
 
 -- printe alle gamle tall hver gang
 
-type OMayT m a = Maybe (m a)
+getNum :: ()
+getNum = undefined
 
-type OStat s m a = Maybe (m a)
-
-type MayT m a = m (Maybe a)
-
-type IoWithError a = ReaderT String (ExceptT String (StateT (Int, [(Int, Int)]) IO)) a
-
-getNum :: IoWithError Int
-getNum = do
-  msg <- ask
-  liftIO $ putStr $ "Input num " <> msg <> ":> "
-  x <- liftIO getLine
-  case readMaybe @Int x of
-    Nothing -> do
-      (succ -> c, xs) <- get
-      liftIO $ putStrLn $ "Error count : " <> show c
-      put (c, xs)
-      throwError $ "invalid number " <> x
-    Just num -> pure num
-
-loop :: IoWithError ()
+loop :: ()
 loop =
-  flip
-    catchError
-    (liftIO . print)
-    ( do
-        prevs <- gets snd
-        liftIO $ putStr "PREVS " >> print prevs
-        a <- getNum
-        b <- getNum
-        modify (\(c, xs) -> (c, (a, b) : xs))
-        liftIO $ print [a .. b]
-    )
-    >> loop
+  undefined
 
 main :: IO ()
 main = do
-  void $ flip evalStateT (0, []) $ runExceptT $ flip runReaderT "MSG" loop
+  undefined
