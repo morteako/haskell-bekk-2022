d0dcdd82035a605125356cb1868df4dbcb3df41c
Morten Kolstad <morten.kolstad@bekk.no>
2023-03-20 17:51:27 +0100

    Refl

--- typefams/Refl.hs
+++ typefams/Refl.hs
@@ -5,6 +5,7 @@
 data a :~: b where
     Refl :: a :~: a
 
+---
 symmetric :: a :~: b -> b :~: a
 symmetric Refl = Refl
 
@@ -33,8 +34,29 @@ type family a + b where
 --     Z + b = b
 --     S n + b = S (n + b)
 
--- zeroIdentityL :: a :~: (Z + a)
--- zeroIdentityL = Refl
+zeroIdentityL :: a :~: (Z + a)
+zeroIdentityL = Refl
 
--- zeroIdentityR :: a :~: (a + Z)
--- zeroIdentityR = Refl
+zeroIdentityR :: forall a. SNat a -> a :~: (a + Z)
+zeroIdentityR SZ = Refl
+zeroIdentityR (SS n) = cong q
+  where
+    q = zeroIdentityR n
+
+-- commutative :: SNat a -> SNat a -> a :~: (a + Z)
+
+data SNat s where
+    SZ :: SNat Z
+    SS :: SNat n -> SNat (S n)
+
+deriving instance Show (SNat Z)
+deriving instance Show (SNat n) => Show (SNat (S n))
+
+class FromNat (s :: Nat) where
+    toSNat :: SNat s
+
+instance FromNat Z where
+    toSNat = SZ
+
+instance FromNat n => FromNat (S n) where
+    toSNat = SS toSNat
